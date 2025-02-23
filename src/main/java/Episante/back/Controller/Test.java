da1f56f5bddac2caee7ba1138fc98eeaa4e5e089
ayoubMah <ayoubelmahjouby9@gmail.com>
2025-02-23 14:34:06 +0100

    last test to ensure that the pipeline works, so modify just one line and see

--- src/main/java/Episante/back/Controller/Test.java
+++ src/main/java/Episante/back/Controller/Test.java
@@ -9,6 +9,6 @@ public class Test {
 
     @GetMapping("/")
     public String Hello(){
-        return "Welcome TO EPISANTE, It's Ayoub";
+        return "Welcome TO EPISANTE, It's Ayoub, Anir and Ikram";
     }
 }
