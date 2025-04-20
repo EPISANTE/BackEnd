af75ece7656aa1b325bfa3de18c66c38e4f58881
ayoubMah <ayoubelmahjouby9@gmail.com>
2025-04-21 01:24:05 +0200

    delete a lot of chaos

--- src/main/java/Episante/back/Repository/DiagnosticSessionRepository.java
+++
@@ -1,9 +0,0 @@
-package Episante.back.Repository;
-
-import Episante.back.Models.DiagnosticSession;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DiagnosticSessionRepository extends JpaRepository<DiagnosticSession, String> {
-}
--- src/main/java/Episante/back/Service/DiagnosticService.java
+++
@@ -1,7 +0,0 @@
-package Episante.back.Service;
-
-public interface DiagnosticService {
-    public String  startSession();
-    public String getNextQuestion(String sessionId);
-    public String processAnswer(String sessionId, String answer);
-}
